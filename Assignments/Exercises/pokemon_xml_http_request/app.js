9d671db38657d39da013f3eb0ab25fabd1d012ec
Eric <ericlingren@gmail.com>
2018-11-01 14:35:33 -0600

    pokemon api

--- Assignments/Exercises/pokemon_xml_http_request/app.js
+++ Assignments/Exercises/pokemon_xml_http_request/app.js
@@ -1,15 +1,33 @@
 var xhr = new XMLHttpRequest();
 
+var myArray = [];
+
 xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
-        var jsonData =xhr.responseText;
+         var jsonData = xhr.responseText;
         var data = JSON.parse(jsonData);
-        //var pokemon = data.pokemon;
-        console.log(data);
-
+        myArray.push(data)
+        sortPokemon();
     }
 };
 
-xhr.open('GET', 'http://api.vschool.io:6543/pokemon.json', true);
+xhr.open('GET', 'https://vschool-cors.herokuapp.com?url=http://api.vschool.io/pokemon', true);
 xhr.send();
 
+function sortPokemon (data){
+
+    var pokemonOnly = myArray[0].objects[0].pokemon
+    //var pokemonObjects = myArray[0].objects[0]
+    //console.log(pokemonOnly);
+
+    pokemonOnly.forEach(function(character){
+        //console.log(name.name);
+        var pokemonName = character.name
+
+        var node = document.createElement('p');
+        var textnode = document.createTextNode(pokemonName);
+        node.appendChild(textnode);
+        document.getElementById('pokemon').appendChild(node);
+        
+    });
+}
