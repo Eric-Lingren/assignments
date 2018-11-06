5bf947944f54dd888d4362bfbc0c4c543e28f811
Eric-Lingren <44101756+Eric-Lingren@users.noreply.github.com>
2018-11-05 22:50:59 -0700

    added complet and delete buttons to toDo list. more styling

--- Assignments/projects/axios_todo/app.js
+++ Assignments/projects/axios_todo/app.js
@@ -17,28 +17,39 @@ function pullArray (listArray){
         toDoContainer.classList.add ('todo');
 
             //  Create HTML elements
-        var title = document.createElement('h2');
+        var title = document.createElement('h3');
             title.classList.add ('description');
         var description = document.createElement('p');
             description.classList.add ('description');
         var isCompleted = document.createElement('p');
             isCompleted.classList.add ('completed');
-        var image = document.createElement('img');
+            //Created a finish button
+        var completeButton = document.createElement('button');
+        var completeText = document.createTextNode('Complete');
+        completeButton.appendChild(completeText);
+            // Creates a delete button
+        var deleteButton = document.createElement('button');
+        var deleteText = document.createTextNode('Delete');
+        deleteButton.appendChild(deleteText);
 
-        image.setAttribute('src', listArray[i].imgUrl)
+        // var image = document.createElement('img');
+
+        // image.setAttribute('src', listArray[i].imgUrl)
 
             //  Put the to-do items inside the div element
         title.textContent = listArray[i].title;
         description.textContent = listArray[i].description;
-        isCompleted.textContent = listArray[i].isCompleted;
+        isCompleted.textContent = listArray[i].completed;
+        
 
-        
-        
             // Put the element on the DOM
         toDoContainer.appendChild(title);
         toDoContainer.appendChild(description);
         toDoContainer.appendChild(isCompleted);
-        toDoContainer.appendChild(image);
+        toDoContainer.appendChild(completeButton);
+        toDoContainer.appendChild(deleteButton);
+
+        // toDoContainer.appendChild(image);
 
         document.getElementById('list-container').appendChild(toDoContainer);
 
@@ -50,26 +61,23 @@ function pullArray (listArray){
     }
 }
 
-var form = document.listForm
+var form = document.listForm;
    
-        form.addEventListener('submit', function(event){
-            event.preventDefault();
-            var inputTitle = form.title.value;
-            var inputDescription = form.description.value;
-            var inputPrice = form.price.value;
-            var inputComplete = form.complete.value;
-            // alert('hey!');
-        var newToDo = {};
-            newToDo.title =  inputTitle;
-            newToDo.description =  inputDescription;
-            newToDo.price =  inputPrice;
-            newToDo.completed =  inputComplete;
-
-             
-        axios.post('https://api.vschool.io/ericlingren/todo', newToDo).then(function(response){
-            console.log(response.data);
-            })
-
-
-        })
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+    var inputTitle = form.title.value;
+    var inputDescription = form.description.value;
+    var inputPrice = form.price.value;
+    var inputComplete = form.complete.value;
+    // alert('hey!');
+var newToDo = {};
+    newToDo.title =  inputTitle;
+    newToDo.description =  inputDescription;
+    newToDo.price =  inputPrice;
+    newToDo.completed =  inputComplete;
+          
+axios.post('https://api.vschool.io/ericlingren/todo', newToDo).then(function(response){
+    console.log(response.data);
+    });
+});
 
