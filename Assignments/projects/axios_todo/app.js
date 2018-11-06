5edf0e917e8a114148be494986f3a22b4730832e
Eric <ericlingren@gmail.com>
2018-11-06 16:06:53 -0700

    to do list added delete and complete.  icon buttons.  image functionality.

--- Assignments/projects/axios_todo/app.js
+++ Assignments/projects/axios_todo/app.js
@@ -1,5 +1,4 @@
 
-
 // var axios = require('axios');
 
 axios.get('https://api.vschool.io/ericlingren/todo').then(function(response){
@@ -16,7 +15,7 @@ function pullArray (listArray){
         var toDoContainer = document.createElement('div');
         toDoContainer.classList.add ('todo');
         var toDoId = listArray[i]._id
-        console.log(toDoId)
+        //console.log(toDoId)
 
             //  Create HTML elements
         var title = document.createElement('h3');
@@ -27,25 +26,75 @@ function pullArray (listArray){
             price.classList.add ('price');
         var isCompleted = document.createElement('p');
             isCompleted.classList.add ('completed');
-            //Created a finish button
-        var completeButton = document.createElement('button');
+
+            //Created a complete item button
+        var completeButton = document.createElement('img');
+        completeButton.setAttribute('src', './complete.png');
+        completeButton.classList.add ('completeButton');
         var completeText = document.createTextNode('Complete');
         completeButton.appendChild(completeText);
+        completeButton.id = toDoId;
+        //creates a function to delete record when clicked
+        completeButton.addEventListener('click', function(){
+            
+            axios.put(`https://api.vschool.io/ericlingren/todo/${this.id}`, { completed: true} ).then(function(response){
+            console.log(response.data);
+                });
+        });
+
+         //  Creates an edit item button
+         var editButton = document.createElement('img');
+         editButton.setAttribute('src', './edit.png');
+         editButton.classList.add ('editButton');
+         var editText = document.createTextNode('edit');
+         editButton.appendChild(editText);
+         editButton.id = toDoId;
+
+        //  creates a function to change the text to an input box
+         editButton.addEventListener('click', function(){
+            console.log(title);
+            console.log(this);
+            //console.log(this.title);
+            //title.style.display='none';
+
+            //  axios.put(`https://api.vschool.io/ericlingren/todo/${this.id}`, { completed: true} ).then(function(response){
+            //  console.log(response.data);
+            //      });
+         });
+ 
+
+        
             // Creates a delete button
-        var deleteButton = document.createElement('button');
-        var deleteText = document.createTextNode('Delete');
+        var deleteButton = document.createElement('img');
+        deleteButton.setAttribute('src', './delete.png');
+        deleteButton.classList.add ('deleteButton');
+        var deleteText = document.createTextNode('delete');
         deleteButton.appendChild(deleteText);
+        deleteButton.id = toDoId;
+            //creates a function to delete record when clicked
+        deleteButton.addEventListener('click', function(){
+            // console.dir(this)
+            axios.delete(`https://api.vschool.io/ericlingren/todo/${this.id}`).then(function(response){
+            console.log(response.data);
+                });
+        });
 
-        // var image = document.createElement('img');
+            //  Adds an image to the div element
+        var image = document.createElement('img');
+        image.setAttribute('src', listArray[i].imgUrl);
+            //  doesnt display the broken image link if the image has no image.
+        if(!listArray[i].imgUrl) {
+            image.style.display='none';
+        } 
 
-        // image.setAttribute('src', listArray[i].imgUrl)
 
             //  Put the to-do items inside the div element
         title.textContent = listArray[i].title;
         description.textContent = listArray[i].description;
         price.textContent = (`Price: ${listArray[i].price}`);
         isCompleted.textContent = (`Completed: ${listArray[i].completed}`);
-        
+
+       
 
             // Put the element on the DOM
         toDoContainer.appendChild(title);
@@ -53,16 +102,20 @@ function pullArray (listArray){
         toDoContainer.appendChild(price);
         toDoContainer.appendChild(isCompleted);
         toDoContainer.appendChild(completeButton);
+        toDoContainer.appendChild(editButton);
         toDoContainer.appendChild(deleteButton);
+        toDoContainer.appendChild(image);
+      
 
         // toDoContainer.appendChild(image);
 
         document.getElementById('list-container').appendChild(toDoContainer);
 
-        //console.log(completed.textContent);
-            //  Check to see if the item has ben completed.  If so, it crosses off the item.
+            //  Check to see if the item has been completed.  If so, it crosses off the item.
         if (isCompleted.textContent === 'Completed: true'){
             title.style.textDecoration = 'line-through'
+            title.style.color = 'green'
+            isCompleted.style.color = 'green'
         }  
     }
 }
@@ -74,18 +127,22 @@ form.addEventListener('submit', function(event){
     var inputTitle = form.title.value;
     var inputDescription = form.description.value;
     var inputPrice = form.price.value;
-    var inputComplete = form.complete.value;
-    // alert('hey!');
+    var inputImage = form.image.value;
+    var inputComplete = form.complete.checked;
+ 
 var newToDo = {};
     newToDo.title =  inputTitle;
     newToDo.description =  inputDescription;
     newToDo.price =  inputPrice;
-    // newToDo.completed =  inputComplete;
+    newToDo.imgUrl =  inputImage;
+    newToDo.completed =  inputComplete;
           
 axios.post('https://api.vschool.io/ericlingren/todo', newToDo).then(function(response){
     console.log(response.data);
     });
 });
+
+
 
 
 
