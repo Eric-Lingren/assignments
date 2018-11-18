72dc9b61fefedf63f1a0567ff786c4098d80b123
Eric-Lingren <44101756+Eric-Lingren@users.noreply.github.com>
2018-11-17 19:55:57 -0700

    palindrome Finder

--- exercises/palindrome_finder/app.js
+++ exercises/palindrome_finder/app.js
@@ -1,10 +1,27 @@
 
 
 function isPalindrome(str){
-
+        //  lowercase
     let strLower = str.toLowerCase();
-    return strLower
+        //  remove whitespaces
+    let strLowerCombined = strLower.replace(/ /g, '');
+        // remove puncuation
+    let noPuncuation = strLowerCombined.replace(/[!,?.]/g, '');
+
+        // convert string to an array to before reverseing it
+    let myArray = noPuncuation.split('');
+        //  Reverse the array
+    let reversed = myArray.reverse();
+        // Join aray to string
+    let strReverse = reversed.join('');
+
+    if (noPuncuation === strReverse){
+        return true
+    } else{
+        return false
+    };
 }
+   
 
 
 console.log(isPalindrome("Star Rats!"));  // true
