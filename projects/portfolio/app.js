3d5b5a42200788aa17fc9fc05685d053d6cd1fd8
Eric-Lingren <44101756+Eric-Lingren@users.noreply.github.com>
2018-11-23 17:28:09 -0700

    portfolio added animation to left and right scroll one notch

--- projects/portfolio/app.js
+++ projects/portfolio/app.js
@@ -3,27 +3,81 @@ const git = document.getElementById('gitLogo')
 const portfolio = document.getElementById('portfolioLogo')
 const linkedin = document.getElementById('linkedinLogo')
 const resume = document.getElementById('resumeLogo')
+const iconBox = document.getElementById('allIcons')
+
+const portfolioTitle = document.getElementById('portfolioTitle')
+const linkedinTitle = document.getElementById('linkedinTitle')
+const gitTitle = document.getElementById('gitTitle')
+
+
+// const exit1 = document.getElementById('exit1')
+// const exit2 = document.getElementById('exit2')
+
+// gitTitle.addEventListener('click', fadeOut);
+
+// function fadeout(){
+//     exit1.style.display = 'block'
+//     exit2.style.display = 'block'
+// }
+
+const winH = window.innerHeight;
+const winW = window.innerWidth;
 
 console.log(resume)
 
 function scrollPortfolioRight() {
     // TweenMax.to(info, .5, {rotationY:-40, transformOrigin:"right 20% 200", transformPerspective:300, x:'40%', y:'', height:'60%'})
     // TweenMax.to(git, .5, {rotationY:00, transformOrigin:"left 20% -1000", transformPerspective:300, x:'100%', y:''})
-    TweenMax.to(portfolio, .5, {rotationY:25, transformOrigin:"left 65% -570", transformPerspective:400, x:'60%', y:'-6%', height:'85%'})
-    TweenMax.to(linkedin, .5, {opacity:0.5})
-    // TweenMax.to(linkedin, .5, {rotationY:50, transformOrigin:"left 20% -250", transformPerspective:400, x:'10%', y:'-5%', height:'50%'})
-    // TweenMax.to(resume, .5, {rotationY:120, transformOrigin:"left 20% -100", transformPerspective:100, x:'-50%', y:'0%'})
+    
+    TweenMax.to(portfolio, .5, {rotationY:55, height:'auto', width:'19%', x:'55%', y:'-3%', translateY:'-3%', transformPerspective: 600, ease: Power1.easeOut});
+
+    gitCenter();
+
+    TweenMax.to(info, .5, {rotationY:-55, height:'auto', width:'19%', x:'15%', y:'-3%', translateY:'-3%', transformPerspective: 600, ease: Power1.easeOut});
+
+    TweenMax.to(linkedin, .5, {rotationY:80, height:'auto', width:'13%', x:'15%', y:'-10%', translateY:'-7%', transformPerspective: 500, ease: Power1.easeOut});
+
+    TweenMax.to(resume, .5, {rotationY:105, height:'auto', width:'8%', x:'-45%', y:'-15%', translateY:'-7%', transformPerspective: 500, ease: Power1.easeOut});
+
+    //TweenMax.to(resume, .5, {opacity:0.7}) 
+}
+
+const linkedinCenter = () => {
+    gitTitle.style.display = 'none'
+    portfolioTitle.style.display = 'none'
+    TweenMax.to(linkedin, .5, {rotationY:0, height:'auto', width:'27%', x:'-48%', y:'0%', translateY:'-3%', transformPerspective: 600, ease: Power1.easeOut, onComplete: function(){linkedinTitle.style.display = 'block'} }); 
+} 
+
+const gitCenter = () => {
+    linkedinTitle.style.display = 'none'
+    portfolioTitle.style.display = 'none'
+    TweenMax.to(git, .5, {rotationY:0, height:'auto', width:'26%', x:'43%', y:'0%', translateY:'-3%', transformPerspective: 600, ease: Power1.easeOut, onComplete: function(){gitTitle.style.display = 'block'}});
 }
 
 function scrollPortfolioLeft() {
-    // TweenMax.to(info, .5, {rotationY:-40, transformOrigin:"right 20% 200", transformPerspective:300, x:'40%', y:'', height:'60%'})
-    // TweenMax.to(git, .5, {rotationY:00, transformOrigin:"left 20% -1000", transformPerspective:300, x:'100%', y:''})
-    TweenMax.to(portfolio, .5, {rotationY:-25, transformOrigin:"right 65% -570", transformPerspective:400, x:'-60%', y:'-6%', height:'85%'})
-    TweenMax.to(git, .5, {opacity:0.5})
-    // TweenMax.to(linkedin, .5, {rotationY:50, transformOrigin:"left 20% -250", transformPerspective:400, x:'10%', y:'-5%', height:'50%'})
-    // TweenMax.to(resume, .5, {rotationY:120, transformOrigin:"left 20% -100", transformPerspective:100, x:'-50%', y:'0%'})
+
+    TweenMax.to(portfolio, .5, 
+        {rotationY:-55, 
+            height:'auto', width:'19%', 
+            x:'-68%', y:'-3%', 
+            translateY:'-3%', 
+            ease: Power1.easeOut, 
+            onComplete: function(){
+                TweenMax.to(portfolio, .01, {transformPerspective: 600, ease: Power1.easeInOut, }) }  
+        }
+    );
+
+    linkedinCenter()
+
+    TweenMax.to(resume, .5, {rotationY:60, height:'auto', width:'16%', x:'-35%', y:'-3%', translateY:'-3%', transformPerspective: 600, ease: Power1.easeOut});
+
+    TweenMax.to(git, .5, {rotationY:-80, height:'auto', width:'13%', x:'-30%', y:'-10%', translateY:'-7%', transformPerspective: 500, ease: Power1.easeOut});
+
+    TweenMax.to(info, .5, {rotationY:-105, height:'auto', width:'10%', x:'20%', y:'-20%', translateY:'-7%', transformPerspective: 500, ease: Power1.easeOut});
+
+    //TweenMax.to(info, .5, {opacity:.5})
+    
 }
 
-// perspective(600px) rotateY(55deg) translateY(-3%)
-linkedin.addEventListener('click', scrollPortfolioRight)
-git.addEventListener('click', scrollPortfolioLeft)
+git.addEventListener('click', scrollPortfolioRight)
+linkedin.addEventListener('click', scrollPortfolioLeft)
