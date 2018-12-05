b72eac0db3d461458dc0fbe0ca27c08b9f50a974
Eric <ericlingren@gmail.com>
2018-12-05 15:52:18 -0700

    basic strategy buttons

--- projects/blackjack/src/TrainBasicStrategy.js
+++ projects/blackjack/src/TrainBasicStrategy.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import axios from 'axios'
-
+let buttonList;
 class TrainBasicStrategy extends Component {
     constructor(){
         super()
@@ -18,6 +18,11 @@ class TrainBasicStrategy extends Component {
             pCard2Number: '',
             dHand: '',
             pHand: '',
+            correctPlay: 'STAND',
+            options: ['HIT', 'STAND', 'DOUBLE', 'SPLIT', 'SURRENDER'],
+            playerGuess: '',
+            buttonList: '',
+            buttonClass: 'checkButton'
         }
     }
     componentDidMount(){
@@ -38,6 +43,9 @@ dealCard = () => {
     const playerCardImage1 = response.data.cards[1].image
     const playerCardImage2 = response.data.cards[2].image
 
+    const hands = ['Hit', 'Stand', 'Double', 'Split', 'Surrender']
+    const options = hands.map(hand => <button className='checkButton' onClick={this.checkButton} id='notSelected' name={hand.toUpperCase()} value={hand.toUpperCase()} >{hand}</button>)
+
       this.setState({
           dealerHand: dealerCardValue,
           dealerImages: dealerCardImage,
@@ -45,6 +53,7 @@ dealCard = () => {
           playerCard2: playerCardValue2,
           playerImage1: playerCardImage1, 
           playerImage2: playerCardImage2,
+          buttonList: options
         }, () => this.showData() )
     })
   }
@@ -137,50 +146,119 @@ dealCard = () => {
 
     if (pHand <= 8 ){
         console.log('the correct Play is HIT')
+        this.setState({
+            correctPlay: 'HIT',
+        })
     } else if (pHand >= 17) {
         console.log('the correct Play is STAND')
+        this.setState({
+            correctPlay: 'STAND',
+        })
     }else if (pHand === 9 && dHand === 2){
         console.log('the correct Play is HIT')
+        this.setState({
+            correctPlay: 'HIT',
+        })
     } else if(pHand <= 9 && dHand <= 6){
         console.log('the correct Play is DOUBLE')
+        this.setState({
+            correctPlay: 'DOUBLE',
+        })
     } else if(pHand === 9 && dHand <= 11){
         console.log('the correct Play is HIT')
+        this.setState({
+            correctPlay: 'HIT',
+        })
     } else if(pHand === 10 && dHand <= 9){
         console.log('the correct Play is DOUBLE')
+        this.setState({
+            correctPlay: 'DOUBLE',
+        })
     } else if (pHand === 10 && dHand <= 11){
         console.log('the correct Play is HIT')
+        this.setState({
+            correctPlay: 'HIT',
+        })
     } else if (pHand === 11 && dHand <= 10){
         console.log('the correct Play is DOUBLE')
+        this.setState({
+            correctPlay: 'DOUBLE',
+        })
     } else if (pHand === 11 && dHand === 11){
         console.log('the correct Play is HIT')
+        this.setState({
+            correctPlay: 'HIT',
+        })
     } else if (pHand === 12 && dHand <= 3){
         console.log('the correct Play is HIT')
+        this.setState({
+            correctPlay: 'HIT',
+        })
     } else if (pHand === 12 && dHand <= 6){
         console.log('the correct Play is STAND')
+        this.setState({
+            correctPlay: 'STAND',
+        })
     } else if(pHand === 12 && dHand <= 11){
         console.log('the correct Play is HIT')
+        this.setState({
+            correctPlay: 'HIT',
+        })
     } else if (pHand === 13 && dHand <= 6){
         console.log('the correct Play is STAND')
+        this.setState({
+            correctPlay: 'STAND',
+        })
     } else if (pHand === 13 && dHand <= 11){
         console.log('the correct Play is HIT')
+        this.setState({
+            correctPlay: 'HIT',
+        })
     } else if (pHand === 14 && dHand <= 6){
         console.log('the correct Play is STAND')
+        this.setState({
+            correctPlay: 'STAND',
+        })
     } else if (pHand === 14 && dHand <= 11){
         console.log('the correct Play is HIT')
+        this.setState({
+            correctPlay: 'HIT',
+        })
     } else if (pHand === 15 && dHand <= 6){
         console.log('the correct Play is STAND')
+        this.setState({
+            correctPlay: 'STAND',
+        })
     } else if (pHand === 15 && dHand <= 9){
         console.log('the correct Play is HIT')
+        this.setState({
+            correctPlay: 'HIT',
+        })
     } else if (pHand === 15 && dHand === 10){
         console.log('the correct Play is SURRENDER (else hit)')
+        this.setState({
+            correctPlay: 'SURRENDER',
+        })
     } else if (pHand === 15 && dHand === 11){
         console.log('the correct Play is HIT')
+        this.setState({
+            correctPlay: 'HIT',
+        })
     } else if (pHand === 16 && dHand <= 6){
         console.log('the correct Play is STAND')
+        this.setState({
+            correctPlay: 'STAND',
+        })
     } else if (pHand === 16 && dHand <= 8){
         console.log('the correct Play is HIT')
+        this.setState({
+            correctPlay: 'HIT',
+        })
     }  else if (pHand === 16 && dHand <= 11){
         console.log('the correct Play is SURRENDER (else hit)')
+        this.setState({
+            correctPlay: 'SURRENDER',
+        })
     }
   }
 
@@ -191,44 +269,104 @@ dealCard = () => {
 
     if (pHand >= 19){
         console.log('the correct play is STAND')
+        this.setState({
+            correctPlay: 'STAND',
+        })
     } else if(pHand === 13 && dHand <= 4){
         console.log('the correct play is HIT')
+        this.setState({
+            correctPlay: 'HIT',
+        })
     } else if (pHand === 13 && dHand <= 6){
         console.log('the correct play is DOUBLE.  Else Hit.')
+        this.setState({
+            correctPlay: 'DOUBLE',
+        })
     } else if(pHand === 13 && dHand <= 11){
         console.log('the correct play is HIT')
+        this.setState({
+            correctPlay: 'HIT',
+        })
     } else if(pHand === 14 && dHand <= 4){
         console.log('the correct play is HIT')
+        this.setState({
+            correctPlay: 'HIT',
+        })
     } else if (pHand === 14 && dHand <= 6){
         console.log('the correct play is DOUBLE.  Else Hit.')
+        this.setState({
+            correctPlay: 'DOUBLE',
+        })
     } else if(pHand === 14 && dHand <= 11){
         console.log('the correct play is HIT')
+        this.setState({
+            correctPlay: 'HIT',
+        })
     } else if(pHand === 15 && dHand <= 3){
         console.log('the correct play is HIT')
+        this.setState({
+            correctPlay: 'HIT',
+        })
     } else if (pHand === 15 && dHand <= 6){
         console.log('the correct play is DOUBLE.  Else Hit.')
+        this.setState({
+            correctPlay: 'DOUBLE',
+        })
     } else if(pHand === 15 && dHand <= 11){
         console.log('the correct play is HIT')
+        this.setState({
+            correctPlay: 'HIT',
+        })
     }  else if(pHand === 16 && dHand <= 3){
         console.log('the correct play is HIT')
+        this.setState({
+            correctPlay: 'HIT',
+        })
     } else if (pHand === 16 && dHand <= 6){
         console.log('the correct play is DOUBLE.  Else Hit.')
+        this.setState({
+            correctPlay: 'DOUBLE',
+        })
     } else if(pHand === 16 && dHand <= 11){
         console.log('the correct play is HIT')
+        this.setState({
+            correctPlay: 'HIT',
+        })
     } else if(pHand === 17 && dHand === 2){
         console.log('the correct play is HIT')
+        this.setState({
+            correctPlay: 'HIT',
+        })
     } else if (pHand === 17 && dHand <= 6){
         console.log('the correct play is DOUBLE.  Else Hit.')
+        this.setState({
+            correctPlay: 'DOUBLE',
+        })
     } else if(pHand === 17 && dHand <= 11){
         console.log('the correct play is HIT')
+        this.setState({
+            correctPlay: 'HIT',
+        })
     } else if(pHand === 18 && dHand === 2){
         console.log('the correct play is STAND')
+        this.setState({
+            correctPlay: 'STAND',
+        })
     } else if (pHand === 18 && dHand <= 6){
         console.log('the correct play is DOUBLE.  Else Stand.')
+        this.setState({
+            correctPlay: 'DOUBLE',
+        })
     } else if(pHand === 18 && dHand <= 8){
         console.log('the correct play is STAND')
+        this.setState({
+            correctPlay: 'STAND',
+        })
     } else if(pHand === 18 && dHand <= 11){
         console.log('the correct play is HIT')
+        this.setState({
+            correctPlay: 'HIT',
+        })
     }
   }
 
@@ -240,39 +378,109 @@ dealCard = () => {
 
     if(pHand === 22){
         console.log('the correct play is SPLIT')
+        this.setState({
+            correctPlay: 'SPLIT',
+        })
     } else if (pHand === 20){
         console.log('the correct play is STAND')
+        this.setState({
+            correctPlay: 'STAND',
+        })
     } else if (pHand === 16){
         console.log('the correct play is SPLIT')
+        this.setState({
+            correctPlay: 'SPLIT',
+        })
     } else if((pHand === 4 || pHand === 6) && dHand <= 7){
         console.log('the correct play is SPLIT')
+        this.setState({
+            correctPlay: 'SPLIT',
+        })
     } else if((pHand === 4 || pHand === 6) && dHand <= 11){
         console.log('the correct play is HIT')
+        this.setState({
+            correctPlay: 'HIT',
+        })
     } else if(pHand === 8 && dHand <= 4){
         console.log('the correct play is HIT')
+        this.setState({
+            correctPlay: 'HIT',
+        })
     } else if (pHand === 8 && dHand <= 6){
         console.log('the correct play is SPLIT')
+        this.setState({
+            correctPlay: 'SPLIT',
+        })
     } else if (pHand === 8 && dHand <= 11){
         console.log('the correct play is HIT')
+        this.setState({
+            correctPlay: 'HIT',
+        })
     } else if (pHand === 12 && dHand <= 6){
         console.log('the correct play is SPLIT')
+        this.setState({
+            correctPlay: 'SPLIT',
+        })
     } else if (pHand === 12 && dHand <= 11){
         console.log('the correct play is HIT')
+        this.setState({
+            correctPlay: 'HIT',
+        })
     } else if (pHand === 14 && dHand <= 7){
         console.log('the correct play is SPLIT')
+        this.setState({
+            correctPlay: 'SPLIT',
+        })
     } else if (pHand === 14 && dHand <= 11){
         console.log('the correct play is HIT')
+        this.setState({
+            correctPlay: 'HIT',
+        })
     } else if (pHand === 18 & (dHand === 7 || dHand === 10 || dHand === 11 ) ){
         console.log('the correct play is STAND')
+        this.setState({
+            correctPlay: 'STAND',
+        })
     }  else if (pHand === 18 && dHand <= 9 ){
         console.log('the correct play is SPLIT')
+        this.setState({
+            correctPlay: 'SPLIT',
+        })
     }
 
 }
 
+checkButton = (e) => {
+    // console.log(e.target.value)
+    //console.log('a check button was clicked')
+    const answer = this.state.correctPlay
+    const guess = e.target.value
+    const hands = ['Hit', 'Stand', 'Double', 'Split', 'Surrender']
+    const butts = hands.map(hand => {
+        if(guess === answer && hand.toUpperCase() === answer){
+            return (
+                <button className='checkButton' onClick={this.checkButton} id='correct' name={hand.toUpperCase()} value={hand.toUpperCase()} >{hand}</button>
+            )
+        }else if (hand.toUpperCase() === answer){
+            return (
+                <button className='checkButton' onClick={this.checkButton} id='correct' name={hand.toUpperCase()} value={hand.toUpperCase()} >{hand}</button>
+            )
+        }else if(guess === hand.toUpperCase()) {
+            return (
+                <button className='checkButton' onClick={this.checkButton} id='wrong' name={hand.toUpperCase()} value={hand.toUpperCase()} >{hand}</button>
+            )
+        }else {
+            return (
+                <button className='checkButton' onClick={this.checkButton} id='notSelected' name={hand.toUpperCase()} value={hand.toUpperCase()} >{hand}</button>
+            )
+        }
+    })
+
+    this.setState({buttonList: butts, buttonClass: 'checkButtonOff'})
+}
+
 
     render() {
-
         return ( 
             <div className='trainingWrapper'>
                 <div className='basicStrategyWrapper'>
@@ -291,13 +499,13 @@ dealCard = () => {
                     <h3 className='handDescription'>Player Hand</h3>
                     <div className='basicStrategyButtonWrapper'>
                         <h2>Choose the correct play:</h2>
-                        <button className='checkButton'>Hit</button>
-                        <button className='checkButton'>Stand</button>
-                        <button className='checkButton'>Double</button>
-                        <button className='checkButton'>Split</button>
-                        <button className='checkButton'>Surrender</button> <br></br>
+                        <button className={this.state.buttonClass} onClick={this.checkButton} id='buttonHit' name='HIT' value='HIT' >Hit</button>
+                        <button className={this.state.buttonClass} onClick={this.checkButton} id='buttonStand' name='STAND' value='STAND' >Stand</button>
+                        <button className={this.state.buttonClass} onClick={this.checkButton} id='buttonDouble' name='DOUBLE' value='DOUBLE' >Double</button>
+                        <button className={this.state.buttonClass} onClick={this.checkButton} id='buttonSplit' name='SPLIT' value='SPLIT' >Split</button>
+                        <button className={this.state.buttonClass} onClick={this.checkButton} id='buttonSurrender' name='SURRENDER' value='SURRENDER' >Surrender</button> <br></br>
+                        {this.state.buttonList}
                         <button className='dealBSButton' onClick={this.dealCard}>Deal</button>
-                        
                     </div>
                     
                 </div>
